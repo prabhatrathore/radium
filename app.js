8f49268ec67d8b8a89e5bc33d0a740e78d900a11
Radhe Radhe <you@example.com>
2021-11-09 13:47:27 +0530

    git commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const obj =  require('./logger');
 const format = require('./formatter');
 const names = require('./util/helper');
-const _=require('lodash');
+const _= require('lodash');
 const obj2 = require("lodash")
 
 obj.getlog();
@@ -20,7 +20,7 @@ console.log(_.chunk(month2,4 ))
 
 console.log(obj2.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy”:Pans Labyrinth"]]))
 
-const array2 = [1,2,5,6,4,9,44,55,25,36,] 
+const array2 = [1,2,5,6,4,9,44,55,25,36] 
 console.log(obj2.tail(array2))
 
 console.log(obj2.union([1,3,5,6],[5,6,9,8],[5,8,94,2]))
